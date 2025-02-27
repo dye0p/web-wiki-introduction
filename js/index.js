a0b2d13220c62fae0775e794e9fbb2f235e31356
DongYeop <s01054830455@gmail.com>
2025-02-27 19:05:01 +0900

    feat: 크루 위키 완성

--- js/index.js
+++ js/index.js
@@ -16,4 +16,30 @@ console.log(
   "color: #d81b60; font-size: 16px; font-weight: bold;"
 );
 
+function inputComment() {
+  alert("댓글이 등록되었습니다");
+
+  var commentInput = document.getElementById("commentInput");
+  var commentText = commentInput.value.trim();
+  var commentDiv = document.createElement("li");
+
+    commentDiv.innerHTML = `
+    <div class="comment-item">
+      <div class="comment-author">
+        <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+        <span>방문자</span>
+      </div>
+      <div class="comment-content">
+        <p>${commentText}</p>
+      </div>
+    </div>
+    `;
+
+    var commentList = document.querySelector(".comment-list");
+    commentList.appendChild(commentDiv); 
+
+    commentInput.value = "";
+
+}
+
 console.log("알맞은 스크립트를 작성하세요");
